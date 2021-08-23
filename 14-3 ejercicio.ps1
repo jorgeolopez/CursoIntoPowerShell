8dc8e0b83555c14d21b89b7fc78218c733b68161
Jorge Lopez <jorgeolopezvasquez@outlook.com>
2021-08-22 20:20:02 -0600

    Corregir problema de logica

--- 14-3 ejercicio.ps1
+++ 14-3 ejercicio.ps1
@@ -3,14 +3,15 @@ Write-Host "Número impares separados por coma"
 [int] $numero = Read-Host "Ingresa un número"
 [string]$mensaje = ""
 for($i= 1;$i -le $numero; $i+=2){
-        #if($num%2 -eq 0){
-
         $mensaje+=[String]$i
-
-        if ($numero-$i -ne 1)
-        {
-        $mensaje+=","
+        if ( ($numero - $i) -eq 1) {
+                break
         }
-   # }
+
+        if (($numero - $i) -ne 0)
+        {
+                $mensaje+=","
+        }
+        
 }
 Write-Host $mensaje
